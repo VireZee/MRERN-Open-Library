3b667f0abdd22d38b96cdaa569340f9ecaa80ac7
Vire <56460839+VireZee@users.noreply.github.com>
2025-03-12 16:03:37 +0700

    redis temp

--- server/src/graphql/resolvers/middleware/Auth.ts
+++ server/src/graphql/resolvers/middleware/Auth.ts
@@ -1,17 +1,33 @@
-import AppDataSource from '../../../DataSource'
-import User from '../../../models/User'
-import { Request } from 'express'
-import { verToken } from '../../../utils/Validation'
+import Redis from '../../../database/Redis.ts'
+import { User } from '../../../models/User.ts'
+import type { Request } from 'express'
+import { verToken } from '../../../utils/Validation.ts'
 import { GraphQLError } from 'graphql'
 
 const Auth = async (_: null, __: {}, context: { req: Request }) => {
     const t = context.req.cookies['!']
     if (!t) throw new GraphQLError('Unauthorized', { extensions: { code: '401' } })
     try {
-        const userRepo = AppDataSource.getRepository(User)
         const decoded = verToken(t)
-        const user = await userRepo.findOne({ where: { user_id: decoded.user_id } })
+        const id = decoded.id
+        const userCache = await Redis.get(`user:${id}`)
+        if (userCache) {
+            const userData = JSON.parse(userCache)
+            return {
+                photo: Buffer.from(userData.photo).toString('base64'),
+                name: userData.name,
+                uname: userData.username,
+                email: userData.email
+            }
+        }
+        const user = await User.findOne({ _id: id })
         if (!user) throw new GraphQLError('Unauthorized', { extensions: { code: '401' } })
+        await Redis.setex(`user:${id}`, 3600, JSON.stringify({
+            photo: user.photo,
+            name: user.name,
+            uname: user.username,
+            email: user.email
+        }))
         return {
             photo: Buffer.from(user.photo).toString('base64'),
             name: user.name,
@@ -19,8 +35,7 @@ const Auth = async (_: null, __: {}, context: { req: Request }) => {
             email: user.email
         }
     } catch (e) {
-        if (e instanceof GraphQLError) throw e
-        else throw new GraphQLError('Internal Server Error', { extensions: { code: '500' } })
+        throw e
     }
 }
 export default Auth
