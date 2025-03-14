ff78b5e0f603d409b68ed1d3e536aaf5f1f64aa3
Vire <56460839+VireZee@users.noreply.github.com>
2025-03-14 15:50:56 +0700

    .

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,24 +1,24 @@
 import type { Store } from '@reduxjs/toolkit'
 import { configureStore } from '@reduxjs/toolkit'
-import AppAct from './slices/core/App'
-import NavAct from './slices/layouts/Navbar'
-import RegAct from './slices/auth/Register'
-import LogAct from './slices/auth/Login'
-import HomeAct from './slices/views/Home'
-import ColAct from './slices/views/Collection'
-import APIAct from './slices/views/API'
-import SetAct from './slices/auth/Settings'
+import App from './slices/core/App'
+import Nav from './slices/layouts/Navbar'
+import Reg from './slices/auth/Register'
+import Log from './slices/auth/Login'
+import Home from './slices/views/Home'
+import Col from './slices/views/Collection'
+import API from './slices/views/API'
+import Set from './slices/auth/Settings'
 
 const ReduxStore: Store = configureStore({
     reducer: {
-        APP: AppAct,
-        NAV: NavAct,
-        REG: RegAct,
-        LOG: LogAct,
-        HOME: HomeAct,
-        COL: ColAct,
-        API: APIAct,
-        SET: SetAct
+        APP: App,
+        NAV: Nav,
+        REG: Reg,
+        LOG: Log,
+        HOME: Home,
+        COL: Col,
+        API: API,
+        SET: Set
     }
 })
 export type RootState = ReturnType<typeof ReduxStore.getState>
