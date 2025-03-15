e6da5ac9ae2e562011a950225db8e619c27cbdaa
Zee <56460839+VireZee@users.noreply.github.com>
2025-03-15 16:36:15 +0700

    col

--- server/src/graphql/resolvers/book/Collection.ts
+++ server/src/graphql/resolvers/book/Collection.ts
@@ -1,43 +1,44 @@
-import { ILike } from 'typeorm'
-import AppDataSource from '../../../DataSource'
-import Col from '../../../models/Collection'
-import { Request } from 'express'
-import { verToken } from '../../../utils/Validation'
-import { GraphQLError } from 'graphql'
+import { Types } from 'mongoose'
+import { Collection } from '../../../models/Collection.ts'
+import type { Request } from 'express'
+import { verifyToken } from '../../../utils/Validation.ts'
 
-const Collection = async (_: null, args: { search: string, page: number }, context: { req: Request }) => {
-    const t = context.req.cookies['!']
-    try {
-        const colRepo = AppDataSource.getRepository(Col)
-        const { user_id } = verToken(t)
-        const { search = '', page } = args
-        const limit = 9
-        const [bookCollection, totalCollection] = await colRepo.findAndCount({
-            where: {
-                user_id,
-                ...(search && { title: ILike(`%${search}%`) })
-            },
-            select: ['author_key', 'cover_edition_key', 'cover_i', 'title', 'author_name'],
-            skip: (page - 1) * limit,
-            take: limit,
-            order: {
-                created: {
-                    direction: 'desc'
-                }
-            }
-        })
-        const found = bookCollection.length
-        const collection = bookCollection.map(book => ({
-            author_key: book.author_key,
-            cover_edition_key: book.cover_edition_key,
-            cover_i: book.cover_i,
-            title: book.title,
-            author_name: book.author_name
-        }))
-        return { found, collection, totalCollection }
-    } catch (e) {
-        if (e instanceof GraphQLError) throw e
-        else throw new GraphQLError('Internal Server Error', { extensions: { code: '500' } })
+interface Query {
+    user_id: Types.ObjectId
+    title?: {
+        $regex: string
+        $options: 'i'
     }
 }
-export default Collection
+const A = async (_: null, args: { search: string, page: number }, context: { req: Request }) => {
+    const t = context.req.cookies['!']
+    try {
+        const { id } = verifyToken(t)
+        const { search = '', page } = args
+        const limit = 9
+        const query: Query = { user_id: id }
+        if (search) query.title = { $regex: search, $options: 'i' }
+        const [bookCollection, totalCollection] = await Promise.all([
+            Collection.find(query)
+                .select('author_key cover_edition_key cover_i title author_name')
+                .sort({ created: -1 })
+                .skip((page - 1) * limit)
+                .limit(limit),
+            Collection.countDocuments(query)
+        ])
+        return {
+            found: bookCollection.length,
+            collection: bookCollection.map(book => ({
+                author_key: book.author_key,
+                cover_edition_key: book.cover_edition_key,
+                cover_i: book.cover_i,
+                title: book.title,
+                author_name: book.author_name
+            })),
+            totalCollection
+        }
+    } catch (e) {
+        throw e
+    }
+}
+export default A
